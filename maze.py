b172480157f72b902a78cb92e4a5355bf0442e53
Praneel Tejpal Bora <praneelbora@gmail.com>
2022-03-02 12:47:20 +0530

    main

--- maze.py
+++ maze.py
@@ -6,10 +6,10 @@ BG = (255,255,255)
 BLACK=(0,0,0)
 FPS=60
 V=5
-MAZE = pygame.image.load("Assets/Images/maze3.png")
+MAZE = pygame.image.load("Assets/Images/maze1.png")
 MAZE = pygame.transform.scale(MAZE,(HEIGHT,HEIGHT))
 
-f = open("Assets/Files/maze3.txt", 'r')
+f = open("Assets/Files/maze1.txt", 'r')
 values = f.readlines()
 start_x = int(values[0])
 start_y = int(values[1])
@@ -39,75 +39,54 @@ def move_dot(keypress,dot):
     
     if(keypress[pygame.K_LEFT] and dot.x-V>150):
         cnt=0
-        i=0
-        while(i<=dot.width):
-            j=0
-            while(j<=dot.height):
+        for i in range(0,dot.width,1):
+            for j in range (0,dot.height,1):
                 color= MAIN.get_at((dot.x+i-V,dot.y+j))
-                if(color[0]<100 and color[1]<100 and color[2]<100) :
+                if(color[0]<100):
                     cnt=1
-                j+=1
-            i+=1
         if(cnt==0):
             dot.x-=V
         
     if(keypress[pygame.K_RIGHT] and dot.x+V<750-dot.width):
         cnt=0
-        i=0
-        while(i<=dot.width):
-            j=0
-            while(j<=dot.height):
+        for i in range(0,dot.width,1):
+            for j in range (0,dot.height,1):
                 color= MAIN.get_at((dot.x+i+V,dot.y+j))
-                if(color[0]<100 and color[1]<100 and color[2]<100) :
+                if(color[0]<100):
                     cnt=1
-                j+=1
-            i+=1
         if(cnt==0):
             dot.x+=V
+
     if(keypress[pygame.K_UP] and dot.y-V>0):
         cnt=0
-        i=0
-        while(i<=dot.width):
-            j=0
-            while(j<=dot.height):
+        for i in range(0,dot.width,1):
+            for j in range (0,dot.height,1):
                 color= MAIN.get_at((dot.x+i,dot.y-V+j))
-                if(color[0]<100 and color[1]<100 and color[2]<100) :
+                if(color[0]<100):
                     cnt=1
-                j+=1
-            i+=1
-
         if(cnt==0):
             dot.y-=V
     if(keypress[pygame.K_DOWN] and dot.y+V<600-dot.height):
         cnt=0
-        i=0
-        while(i<=dot.width):
-            j=0
-            while(j<=dot.height):
+        for i in range(0,dot.width,1):
+            for j in range (0,dot.height,1):
                 color= MAIN.get_at((dot.x+i,dot.y+V+j))
-                if(color[0]<100 and color[1]<100 and color[2]<100) :
+                if(color[0]<100):
                     cnt=1
-                j+=1
-            i+=1
         if(cnt==0):
             dot.y+=V
 def check(dot):
     if(dot.x>end_x_1 and dot.x<end_x_2 and dot.y>end_y_1 and dot.y<end_y_2):
-        
-        i=0
         clock = pygame.time.Clock()
-        while(i<90):
-            clock.tick(FPS*3000)
+        for i in range(0,90,1):
+            clock.tick(FPS)
             if(end_axi==0):
-                
                 dot.x+=end_mov
             else:
-                
                 dot.y+=end_mov
             draw(dot,1)
-            i+=1
         pygame.quit()
-    
+
 
 def main():
     running = True
